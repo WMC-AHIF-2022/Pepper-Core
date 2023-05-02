f68437b20dd143d05d536e9734eeb3c0913ef428
David-Proeller <86597963+David-Proeller@users.noreply.github.com>
2023-05-02 16:05:10 +0200

    Database

--- src/client/index.js
+++ src/client/index.js
@@ -1,39 +1,53 @@
-import { fetchRestEndpoint } from "./utils/client-server.js";
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const client_server_js_1 = require("./utils/client-server.js");
 const btnLogin = document.getElementById("btnLogin");
 const btnSignup = document.getElementById("btnSignup");
 if (btnSignup) {
-    btnSignup.addEventListener("click", async () => await signup());
+    btnSignup.addEventListener("click", () => __awaiter(void 0, void 0, void 0, function* () { return yield signup(); }));
 }
 if (btnLogin) {
-    btnLogin.addEventListener("click", async () => await login());
+    btnLogin.addEventListener("click", () => __awaiter(void 0, void 0, void 0, function* () { return yield login(); }));
 }
-async function login() {
-    try {
-        const elementUsername = document.getElementById("username");
-        const username = elementUsername.value;
-        const elementPassword = document.getElementById("password");
-        const password = elementPassword.value;
-        const data = JSON.parse(`{"username": "${username}", "password": "${password}"}`);
-        await fetchRestEndpoint("http://localhost:3333/api/users/login", "POST", data);
-        console.log("Yessir");
-        sessionStorage.setItem('chat-user', username);
-    }
-    catch (e) {
-        alert(e);
-    }
+function login() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const elementUsername = document.getElementById("username");
+            const username = elementUsername.value;
+            const elementPassword = document.getElementById("password");
+            const password = elementPassword.value;
+            const data = JSON.parse(`{"username": "${username}", "password": "${password}"}`);
+            yield (0, client_server_js_1.fetchRestEndpoint)("http://localhost:3333/api/users/login", "POST", data);
+            console.log("Yessir");
+            sessionStorage.setItem('chat-user', username);
+        }
+        catch (e) {
+            alert(e);
+        }
+    });
 }
-async function signup() {
-    try {
-        const elementUsername = document.getElementById("username");
-        const username = elementUsername.value;
-        const elementPassword = document.getElementById("password");
-        const password = elementPassword.value;
-        const data = JSON.parse(`{"username": "${username}", "password": "${password}"}`);
-        await fetchRestEndpoint("http://localhost:3333/api/users/signup", "POST", data);
-        console.log("Yessir");
-    }
-    catch (e) {
-        alert(e);
-    }
+function signup() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const elementUsername = document.getElementById("username");
+            const username = elementUsername.value;
+            const elementPassword = document.getElementById("password");
+            const password = elementPassword.value;
+            const data = JSON.parse(`{"username": "${username}", "password": "${password}"}`);
+            yield (0, client_server_js_1.fetchRestEndpoint)("http://localhost:3333/api/users/signup", "POST", data);
+            console.log("Yessir");
+        }
+        catch (e) {
+            alert(e);
+        }
+    });
 }
-//# sourceMappingURL=index.js.map
--- src/client/utils/client-server.js
+++ src/client/utils/client-server.js
@@ -1,16 +1,30 @@
-export async function fetchRestEndpoint(route, httpMethod, data) {
-    let options = { method: httpMethod };
-    if (data) {
-        options.headers = { "Content-Type": "application/json" };
-        options.body = JSON.stringify(data);
-    }
-    const response = await fetch(route, options);
-    if (response.ok) {
-        return response;
-    }
-    else {
-        const errorMessage = await response.text();
-        throw new Error(errorMessage);
-    }
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.fetchRestEndpoint = void 0;
+function fetchRestEndpoint(route, httpMethod, data) {
+    return __awaiter(this, void 0, void 0, function* () {
+        let options = { method: httpMethod };
+        if (data) {
+            options.headers = { "Content-Type": "application/json" };
+            options.body = JSON.stringify(data);
+        }
+        const response = yield fetch(route, options);
+        if (response.ok) {
+            return response;
+        }
+        else {
+            const errorMessage = yield response.text();
+            throw new Error(errorMessage);
+        }
+    });
 }
-//# sourceMappingURL=client-server.js.map
+exports.fetchRestEndpoint = fetchRestEndpoint;
